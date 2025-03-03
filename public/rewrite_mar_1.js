5a7ded96a3fac2b8f3eea801d07afaf9dfd004ba
Liam Swayne <liamtswayne@gmail.com>
2025-03-02 20:59:17 -0500

    add more calendar rendering

--- public/rewrite_mar_1.js
+++ public/rewrite_mar_1.js
@@ -112,6 +112,7 @@ function getDay(offset) {
 let SETTINGS = {
     stacking: undefined,
     numberOfCalendarDays: undefined,
+    ampmOr24: undefined
 }
 
 if (localStorage.getItem("numberOfCalendarColumns") == null) {
@@ -120,10 +121,23 @@ if (localStorage.getItem("numberOfCalendarColumns") == null) {
     SETTINGS.numberOfCalendarDays = localStorage.getItem("numberOfCalendarColumns");
 }
 
-let gapBetweenColumns = 4
+if (localStorage.getItem("ampmOr24") == null) {
+    SETTINGS.ampmOr24 = 'ampm';
+} else {
+    SETTINGS.ampmOr24 = localStorage.getItem("ampmOr24");
+}
+
+let gapBetweenColumns = 6;
 let windowBorderMargin = 6;
 let columnWidth; // portion of screen
 let headerSpace = 26; // px gap at top to make space for logo and buttons
+
+let adjustCalendarUp; // px to adjust calendar up by based on browser
+if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+    adjustCalendarUp = 0;
+} else {
+    adjustCalendarUp = 2;
+}
 
 if (localStorage.getItem("SETTINGS.stacking") == null) {
     SETTINGS.stacking = false
@@ -132,7 +146,7 @@ if (localStorage.getItem("SETTINGS.stacking") == null) {
 }
 
 let currentDays = [];
-for (i = 0; i < SETTINGS.numberOfCalendarDays; i++) {
+for (let i = 0; i < SETTINGS.numberOfCalendarDays; i++) {
     currentDays.push(getDay(i));
 }
 
@@ -152,49 +166,124 @@ function numberOfColumns() {
     }
 }
 
-function renderDay(day, element) {
+function nthHourText(n) {
+    ASSERT(Number.isInteger(n), "nthHourText n must be an integer");
+    ASSERT(0 <= n && n < 24, "nthHourText n out of range 0-23");
+    ASSERT(SETTINGS.ampmOr24 === 'ampm' || SETTINGS.ampmOr24 === '24', "SETTINGS.ampmOr24 must be 'ampm' or '24'");
+    if (SETTINGS.ampmOr24 == '24') {
+        return String(n);
+    } else { // ampm
+        if (n == 0) {
+            return '12am';
+        } else if (n == 12) {
+            return '12pm';
+        } else if (n < 12) {
+            return String(n) + 'am';
+        } else {
+            return String(n-12) + 'pm';
+        }
+    }
+}
+
+function renderDay(day, element, index) {
     // get existing element
     ASSERT(day != undefined && day != null, "renderDay day is undefined or null");
     ASSERT(/^\d{4}-\d{2}-\d{2}$/.test(day), "day doesn't fit YYYY-MM-DD format");
     ASSERT(element != undefined && element != null, "renderDay element is undefined or null");
-    ASSERT(element.style.width == String(columnWidth) + 'px', `renderDay element width (${element.style.width}) is not ${columnWidth}`);
+    ASSERT(parseFloat(element.style.width.slice(0, -2)).toFixed(2) == columnWidth.toFixed(2), `renderDay element width (${parseFloat(element.style.width.slice(0, -2)).toFixed(2)}) is not ${columnWidth.toFixed(2)}`);
     ASSERT(!isNaN(columnWidth), "columnWidth must be a number");
     ASSERT(element.style.height != undefined && element.style.height != null, "element height is undefined or null");
-    ASSERT(element.style.height.slice(element.style.height.length-2, element.style.height.length) == 'px', `element height last 2 chars aren't 'px': ${element.style.height.slice(element.style.height.length, element.style.height.length-2)}`);
-    ASSERT(!isNaN(parseFloat(element.style.height.slice(0, -2))), "element height is not a float");
+    ASSERT(element.style.height.slice(element.style.height.length-2, element.style.height.length) == 'px', `element height last 2 chars aren't 'px': ${element.style.height.slice(element.style.height.length-2, element.style.height.length)}`);
+    ASSERT(!isNaN(parseFloat(element.style.height.slice(0, -2))), "element height is not a number");
 
     // look for hour markers
-    if (document.getElementById(`day${i}hourMarker1`) == null) { // create 24 hour markers
+    if (document.getElementById(`day${index}hourMarker1`) == null) { // create hour markers
         // if one is missing, all 24 must be missing
-        for (i = 0; i < 24; i++) {
-            ASSERT(document.getElementById(`day${i}hourMarker${i}`) == null, `hourMarker${i} exists but hourMarker${i} doesn't`);
+        for (let j = 1; j < 24; j++) { // skip first because there's a top line
+            ASSERT(document.getElementById(`day${index}hourMarker${j}`) == null, `hourMarker1 exists but hourMarker${j} doesn't`);
             let hourMarker = document.createElement('div');
-            hourMarker.id = `day${i}hourMarker${i}`;
+            hourMarker.id = `day${index}hourMarker${j}`;
             hourMarker.style.position = 'fixed';
-            hourMarker.style.width = String(columnWidth) + 'px';
+            hourMarker.style.width = String(columnWidth + 1) + 'px';
             hourMarker.style.height = '1px';
             hourMarker.backgroundColor = '#000';
             ASSERT(element.style.top != undefined && element.style.top != null, "element.style.top is undefined or null");
-            // assert that element.style.top is string of int followed by 'px'
             ASSERT(/^\d+px$/.test(element.style.top), "element.style.top is not a string of int followed by 'px'");
             let dayElementVerticalPos = parseInt(element.style.top.slice(0, -2));
-            // same asserts for height
+            ASSERT(element.style.left.slice(element.style.left.length-2, element.style.left.length) == 'px', `element style 'left' last 2 chars aren't 'px': ${element.style.left.slice(element.style.left.length-2, element.style.left.length)}`);
+            ASSERT(!isNaN(parseFloat(element.style.left.slice(0, -2))), "element height is not a number");
+            let dayElementHorizontalPos = parseInt(element.style.left.slice(0, -2));
             ASSERT(element.style.height != undefined && element.style.height != null, "element.style.height is undefined or null");
             ASSERT(element.style.height.slice(element.style.height.length-2, element.style.height.length) == 'px', `element height last 2 chars aren't 'px': ${element.style.height.slice(element.style.height.length, element.style.height.length-2)}`);
-            ASSERT(!isNaN(parseFloat(element.style.height.slice(0, -2))), "element height is not a float");
+            ASSERT(!isNaN(parseFloat(element.style.height.slice(0, -2))), "element height is not a number");
             let dayHeight = parseFloat(element.style.height.slice(0, -2));
-            hourMarker.style.top = String(dayElementVerticalPos + (i * dayHeight / 24)) + 'px';
+            hourMarker.style.top = String(dayElementVerticalPos + (j * dayHeight / 24)) + 'px';
+            hourMarker.style.left = String(dayElementHorizontalPos + 1) + 'px';
+            hourMarker.style.backgroundColor = '#000';
+            document.body.appendChild(hourMarker);
+
+            // create hour marker text
+            ASSERT(document.getElementById(`day${index}hourMarkerText${j}`) == null, `hourMarkerText1 exists but hourMarkerText${j} doesn't`);
+            let hourMarkerText = document.createElement('div');
+            hourMarkerText.id = `day${index}hourMarkerText${j}`;
+            hourMarkerText.style.position = 'fixed';
+
+            hourMarkerText.style.top = String(dayElementVerticalPos + (j * dayHeight / 24) + 2) + 'px';
+            hourMarkerText.style.left = String(dayElementHorizontalPos + 4) + 'px';
+            hourMarkerText.style.color = '#000';
+            hourMarkerText.style.fontSize = '10px';
+            hourMarkerText.innerHTML = nthHourText(j);
+            document.body.appendChild(hourMarkerText);
         }
+
+        // first hour (text only)
+        let hourMarkerText = document.createElement('div');
+        hourMarkerText.id = `day${index}hourMarkerText0`;
+        hourMarkerText.style.position = 'fixed';
+        let dayElementVerticalPos = parseInt(element.style.top.slice(0, -2));
+        hourMarkerText.style.top = String(dayElementVerticalPos + 2) + 'px';
+        let dayElementHorizontalPos = parseInt(element.style.left.slice(0, -2));
+        hourMarkerText.style.left = String(dayElementHorizontalPos + 4) + 'px';
+        hourMarkerText.style.color = '#000';
+        hourMarkerText.style.fontSize = '10px';
+        hourMarkerText.innerHTML = nthHourText(0);
+        document.body.appendChild(hourMarkerText);
+    } else { // update hour markers
+        for (let j = 1; j < 24; j++) {
+            // adjust position of hour markers
+            let hourMarker = document.getElementById(`day${index}hourMarker${j}`);
+            ASSERT(hourMarker != null, `hourMarker1 exists but hourMarker${j} doesn't`);
+            let dayElementVerticalPos = parseInt(element.style.top.slice(0, -2));
+            let dayElementHorizontalPos = parseInt(element.style.left.slice(0, -2));
+            let dayHeight = parseFloat(element.style.height.slice(0, -2));
+            hourMarker.style.top = String(dayElementVerticalPos + (j * dayHeight / 24)) + 'px';
+            hourMarker.style.left = String(dayElementHorizontalPos + 1) + 'px';
+
+            // update width of hour markers
+            hourMarker.style.width = String(columnWidth + 1) + 'px';
+
+            // adjust position of hour marker text
+            let hourMarkerText = document.getElementById(`day${index}hourMarkerText${j}`);
+            ASSERT(hourMarkerText != null, `hourMarkerText1 exists but hourMarkerText${j} doesn't`);
+            hourMarkerText.style.top = String(dayElementVerticalPos + (j * dayHeight / 24) + 2) + 'px';
+            hourMarkerText.style.left = String(dayElementHorizontalPos + 4) + 'px';
+        }
+
+        // first hour (text only)
+        let hourMarkerText = document.getElementById(`day${index}hourMarkerText0`);
+        let dayElementVerticalPos = parseInt(element.style.top.slice(0, -2));
+        hourMarkerText.style.top = String(dayElementVerticalPos + 2) + 'px';
+        let dayElementHorizontalPos = parseInt(element.style.left.slice(0, -2));
+        hourMarkerText.style.left = String(dayElementHorizontalPos + 4) + 'px';
     }
 }
 
 function renderCalendar(days) {
     ASSERT(days != undefined && days != null && days.length == SETTINGS.numberOfCalendarDays)
-
+    ASSERT(SETTINGS.stacking == true || SETTINGS.stacking == false, "SETTINGS.stacking must be a boolean");
     if (SETTINGS.stacking) { // vertically stack each 2 days and task list
-
     } else { // no vertical stacking
-        for (i = 0; i < 7; i++) {
+        for (let i = 0; i < 7; i++) {
             if (i >= SETTINGS.numberOfCalendarDays) {
                 // remove excess day elements
                 let dayElement = document.getElementById('day' + String(i));
@@ -206,22 +295,25 @@ function renderCalendar(days) {
 
             let dayElement = document.getElementById('day' + String(i));
             if (dayElement == null) {
-                // create new element
-                dayElement = document.createElement('div');
-                dayElement.id = 'day' + String(i);
-                dayElement.style.position = 'fixed';
-                dayElement.style.width = String(columnWidth) + 'px';
-                dayElement.style.height = String(window.innerHeight - (2 * windowBorderMargin) - headerSpace) + 'px';
-                dayElement.style.top = String(windowBorderMargin) + 'px';
-                dayElement.style.left = String(windowBorderMargin + ((columnWidth + gapBetweenColumns) * i)) + 'px';
+                dayElement = document.createElement('div'); // create new element
             }
-            renderDay(days[i], dayElement);
+            dayElement.id = 'day' + String(i);
+            dayElement.style.position = 'fixed';
+            dayElement.style.width = String(columnWidth) + 'px';
+            dayElement.style.height = String(window.innerHeight - (2 * windowBorderMargin) - headerSpace - adjustCalendarUp) + 'px';
+            dayElement.style.top = String(windowBorderMargin + headerSpace) + 'px';
+            dayElement.style.left = String(windowBorderMargin + ((columnWidth + gapBetweenColumns) * (i+1))) + 'px'; // i+1 because first column is task list
+            dayElement.style.border = '1px solid #000';
+            dayElement.style.borderRadius = '5px';
+            document.body.appendChild(dayElement);
+
+            renderDay(days[i], dayElement, i);
         }
     }
 }
 
 function resizeListener() {
-    columnWidth = ((window.innerWidth - windowBorderMargin - 6) / (numberOfColumns() + 1));
+    columnWidth = ((window.innerWidth - (2*windowBorderMargin) - gapBetweenColumns*(numberOfColumns() - 1)) / numberOfColumns()); // 1 fewer gaps than columns
     ASSERT(!isNaN(columnWidth), "columnWidth must be a float");
     renderCalendar(currentDays);
 }
