e31c28f140800f5d4d8b1dbdb36d07b22a427aa7
Liam Swayne <liamtswayne@gmail.com>
2025-07-08 12:16:29 -0400

    .

--- browser_complete/private/server/main.js
+++ browser_complete/private/server/main.js
@@ -1,3 +1,26 @@
+function SEND(data, status = 200, headers = {}) {
+    const corsHeaders = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, HEAD, POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    };
+
+    if (typeof data === 'object' && data !== null) {
+        data = JSON.stringify(data);
+        if (!headers['Content-Type']) {
+            headers['Content-Type'] = 'application/json';
+        }
+    }
+
+    return new Response(data, {
+        status,
+        headers: {
+            ...corsHeaders,
+            ...headers
+        },
+    });
+}
+
 export default {
     async fetch(request, env, ctx) {
         return new Response('Hello World!');
